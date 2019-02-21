0dcbebc46b0fa01ead8149f8f46a8445c2b64b01
Falia <math.1510@hotmail.fr>
2019-02-21 13:51:16 +0100

    clear data, set a maximum of songs and order by name

--- js/app.js
+++ js/app.js
@@ -28,6 +28,20 @@ function getRandomInArray(arr){
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
+function getMultipleRandom(arr, n) {
+    var result = new Array(n),
+        len = arr.length,
+        taken = new Array(len);
+    if (n > len)
+        throw new RangeError("getRandom: more elements taken than available");
+    while (n--) {
+        var x = Math.floor(Math.random() * len);
+        result[n] = arr[x in taken ? taken[x] : x];
+        taken[x] = --len in taken ? taken[len] : len;
+    }
+    return result;
+}
+
 function checkGuess(e, randomMusic){
     if(e.id == randomMusic.id){
         alert("GG WP");
@@ -36,15 +50,30 @@ function checkGuess(e, randomMusic){
     }
 }
 
+function hydrate(data){
+    songs = getMultipleRandom(data, 9);
+    songs.sort( (a, b) => {
+        var x = a.name.toLowerCase();
+        var y = b.name.toLowerCase();
+        return x < y ? -1 : x > y ? 1 : 0;
+    });
+
+    return songs;
+}
+
 getMusicList().then(data => {
     // create music title's block
     var parent = document.getElementById('music_block');
-    data.forEach((e) => {
+
+    songs = hydrate(data, 9);
+
+    songs.forEach((e) => {
         createChild(parent, e.name, e.id);
     });
 
     // get random music and set it to the youtube iframe
-    let randomMusic = getRandomInArray(data);
+    let randomMusic = getRandomInArray(songs);
+    console.log(randomMusic);
     var iframe = document.getElementById("youtube");
     iframe.setAttribute("src", "https://www.youtube.com/embed/" + randomMusic.url + '?autoplay=1');
 
